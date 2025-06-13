256f2492467f33e7576929b1a8938bf722c491bf
Angel14 <20250443@ricaldone.edu.sv>
2025-06-13 09:19:04 -0600

    form completo

--- promedio/Form1.Designer.cs
+++ promedio/Form1.Designer.cs
@@ -28,35 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.gbNotas = new System.Windows.Forms.GroupBox();
-            this.gbPromedio = new System.Windows.Forms.GroupBox();
-            this.lblNotaUno = new System.Windows.Forms.Label();
-            this.lblNotaDos = new System.Windows.Forms.Label();
-            this.lblNotaTres = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtNotaTres = new System.Windows.Forms.TextBox();
+            this.txtNotaDos = new System.Windows.Forms.TextBox();
+            this.txtNotaUno = new System.Windows.Forms.TextBox();
             this.lblError = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lblNotaTres = new System.Windows.Forms.Label();
+            this.lblNotaDos = new System.Windows.Forms.Label();
+            this.lblNotaUno = new System.Windows.Forms.Label();
+            this.gbPromedio = new System.Windows.Forms.GroupBox();
             this.lblPromedio = new System.Windows.Forms.Label();
+            this.txtPromedioFinal = new System.Windows.Forms.TextBox();
+            this.gbRegistros = new System.Windows.Forms.GroupBox();
             this.btnSalir = new System.Windows.Forms.Button();
             this.btnNuevoRegistro = new System.Windows.Forms.Button();
             this.btnAgregarPromedio = new System.Windows.Forms.Button();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.tlpAyuda = new System.Windows.Forms.ToolStrip();
+            this.tlpAyuda = new System.Windows.Forms.ToolTip(this.components);
             this.gbNotas.SuspendLayout();
-            this.gbPromedio.SuspendLayout();
-            this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.gbPromedio.SuspendLayout();
+            this.gbRegistros.SuspendLayout();
             this.SuspendLayout();
             // 
             // gbNotas
             // 
             this.gbNotas.Controls.Add(this.pictureBox2);
-            this.gbNotas.Controls.Add(this.textBox3);
-            this.gbNotas.Controls.Add(this.textBox2);
-            this.gbNotas.Controls.Add(this.textBox1);
+            this.gbNotas.Controls.Add(this.txtNotaTres);
+            this.gbNotas.Controls.Add(this.txtNotaDos);
+            this.gbNotas.Controls.Add(this.txtNotaUno);
             this.gbNotas.Controls.Add(this.lblError);
             this.gbNotas.Controls.Add(this.lblNotaTres);
             this.gbNotas.Controls.Add(this.lblNotaDos);
@@ -69,35 +70,49 @@
             this.gbNotas.TabStop = false;
             this.gbNotas.Text = "Registro de notas";
             // 
-            // gbPromedio
+            // pictureBox2
             // 
-            this.gbPromedio.Controls.Add(this.lblPromedio);
-            this.gbPromedio.Controls.Add(this.textBox4);
-            this.gbPromedio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gbPromedio.Location = new System.Drawing.Point(39, 315);
-            this.gbPromedio.Name = "gbPromedio";
-            this.gbPromedio.Size = new System.Drawing.Size(699, 100);
-            this.gbPromedio.TabIndex = 1;
-            this.gbPromedio.TabStop = false;
-            this.gbPromedio.Text = "Promedio";
+            this.pictureBox2.Image = global::promedio.Properties.Resources.icons8_icono_de_usuario_100;
+            this.pictureBox2.Location = new System.Drawing.Point(356, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 91);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
             // 
-            // lblNotaUno
+            // txtNotaTres
             // 
-            this.lblNotaUno.AutoSize = true;
-            this.lblNotaUno.Location = new System.Drawing.Point(37, 47);
-            this.lblNotaUno.Name = "lblNotaUno";
-            this.lblNotaUno.Size = new System.Drawing.Size(67, 20);
-            this.lblNotaUno.TabIndex = 0;
-            this.lblNotaUno.Text = "Nota 1:";
+            this.txtNotaTres.Location = new System.Drawing.Point(113, 135);
+            this.txtNotaTres.Name = "txtNotaTres";
+            this.txtNotaTres.Size = new System.Drawing.Size(118, 26);
+            this.txtNotaTres.TabIndex = 6;
+            this.tlpAyuda.SetToolTip(this.txtNotaTres, "Ingresa valores entre 1 y 10");
             // 
-            // lblNotaDos
+            // txtNotaDos
             // 
-            this.lblNotaDos.AutoSize = true;
-            this.lblNotaDos.Location = new System.Drawing.Point(40, 92);
-            this.lblNotaDos.Name = "lblNotaDos";
-            this.lblNotaDos.Size = new System.Drawing.Size(67, 20);
-            this.lblNotaDos.TabIndex = 1;
-            this.lblNotaDos.Text = "Nota 2:";
+            this.txtNotaDos.Location = new System.Drawing.Point(113, 92);
+            this.txtNotaDos.Name = "txtNotaDos";
+            this.txtNotaDos.Size = new System.Drawing.Size(118, 26);
+            this.txtNotaDos.TabIndex = 5;
+            this.tlpAyuda.SetToolTip(this.txtNotaDos, "Ingresa valores entre 1 y 10");
+            // 
+            // txtNotaUno
+            // 
+            this.txtNotaUno.Location = new System.Drawing.Point(113, 47);
+            this.txtNotaUno.Name = "txtNotaUno";
+            this.txtNotaUno.Size = new System.Drawing.Size(118, 26);
+            this.txtNotaUno.TabIndex = 4;
+            this.tlpAyuda.SetToolTip(this.txtNotaUno, "Ingresa valores entre 1 y 10");
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(40, 217);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(49, 20);
+            this.lblError.TabIndex = 3;
+            this.lblError.Text = "Error";
             // 
             // lblNotaTres
             // 
@@ -108,57 +123,35 @@
             this.lblNotaTres.TabIndex = 2;
             this.lblNotaTres.Text = "Nota 3:";
             // 
-            // groupBox1
+            // lblNotaDos
             // 
-            this.groupBox1.Controls.Add(this.btnSalir);
-            this.groupBox1.Controls.Add(this.btnNuevoRegistro);
-            this.groupBox1.Controls.Add(this.btnAgregarPromedio);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(615, 30);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(173, 279);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Registros";
+            this.lblNotaDos.AutoSize = true;
+            this.lblNotaDos.Location = new System.Drawing.Point(40, 92);
+            this.lblNotaDos.Name = "lblNotaDos";
+            this.lblNotaDos.Size = new System.Drawing.Size(67, 20);
+            this.lblNotaDos.TabIndex = 1;
+            this.lblNotaDos.Text = "Nota 2:";
             // 
-            // lblError
+            // lblNotaUno
             // 
-            this.lblError.AutoSize = true;
-            this.lblError.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lblError.ForeColor = System.Drawing.Color.Red;
-            this.lblError.Location = new System.Drawing.Point(40, 217);
-            this.lblError.Name = "lblError";
-            this.lblError.Size = new System.Drawing.Size(49, 20);
-            this.lblError.TabIndex = 3;
-            this.lblError.Text = "Error";
+            this.lblNotaUno.AutoSize = true;
+            this.lblNotaUno.Location = new System.Drawing.Point(37, 47);
+            this.lblNotaUno.Name = "lblNotaUno";
+            this.lblNotaUno.Size = new System.Drawing.Size(67, 20);
+            this.lblNotaUno.TabIndex = 0;
+            this.lblNotaUno.Text = "Nota 1:";
             // 
-            // textBox1
+            // gbPromedio
             // 
-            this.textBox1.Location = new System.Drawing.Point(113, 47);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(118, 26);
-            this.textBox1.TabIndex = 4;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(113, 92);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(118, 26);
-            this.textBox2.TabIndex = 5;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(113, 135);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(118, 26);
-            this.textBox3.TabIndex = 6;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(160, 55);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 26);
-            this.textBox4.TabIndex = 0;
+            this.gbPromedio.Controls.Add(this.lblPromedio);
+            this.gbPromedio.Controls.Add(this.txtPromedioFinal);
+            this.gbPromedio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbPromedio.Location = new System.Drawing.Point(39, 315);
+            this.gbPromedio.Name = "gbPromedio";
+            this.gbPromedio.Size = new System.Drawing.Size(699, 100);
+            this.gbPromedio.TabIndex = 1;
+            this.gbPromedio.TabStop = false;
+            this.gbPromedio.Text = "Promedio";
             // 
             // lblPromedio
             // 
@@ -168,6 +161,26 @@
             this.lblPromedio.Size = new System.Drawing.Size(128, 20);
             this.lblPromedio.TabIndex = 1;
             this.lblPromedio.Text = "Promedio final:";
+            // 
+            // txtPromedioFinal
+            // 
+            this.txtPromedioFinal.Location = new System.Drawing.Point(160, 55);
+            this.txtPromedioFinal.Name = "txtPromedioFinal";
+            this.txtPromedioFinal.Size = new System.Drawing.Size(366, 26);
+            this.txtPromedioFinal.TabIndex = 0;
+            // 
+            // gbRegistros
+            // 
+            this.gbRegistros.Controls.Add(this.btnSalir);
+            this.gbRegistros.Controls.Add(this.btnNuevoRegistro);
+            this.gbRegistros.Controls.Add(this.btnAgregarPromedio);
+            this.gbRegistros.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbRegistros.Location = new System.Drawing.Point(615, 30);
+            this.gbRegistros.Name = "gbRegistros";
+            this.gbRegistros.Size = new System.Drawing.Size(173, 279);
+            this.gbRegistros.TabIndex = 2;
+            this.gbRegistros.TabStop = false;
+            this.gbRegistros.Text = "Registros";
             // 
             // btnSalir
             // 
@@ -179,6 +192,7 @@
             this.btnSalir.TabIndex = 2;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // btnNuevoRegistro
             // 
@@ -191,6 +205,7 @@
             this.btnNuevoRegistro.Text = "Nuevo Registro";
             this.btnNuevoRegistro.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnNuevoRegistro.UseVisualStyleBackColor = true;
+            this.btnNuevoRegistro.Click += new System.EventHandler(this.btnNuevoRegistro_Click);
             // 
             // btnAgregarPromedio
             // 
@@ -203,43 +218,30 @@
             this.btnAgregarPromedio.Text = "Agregar promedio";
             this.btnAgregarPromedio.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnAgregarPromedio.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::promedio.Properties.Resources.icons8_icono_de_usuario_100;
-            this.pictureBox2.Location = new System.Drawing.Point(356, 27);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 91);
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
+            this.btnAgregarPromedio.Click += new System.EventHandler(this.btnAgregarPromedio_Click);
             // 
             // tlpAyuda
             // 
-            this.tlpAyuda.Location = new System.Drawing.Point(0, 0);
-            this.tlpAyuda.Name = "tlpAyuda";
-            this.tlpAyuda.Size = new System.Drawing.Size(800, 25);
-            this.tlpAyuda.TabIndex = 3;
-            this.tlpAyuda.Text = "toolStrip1";
+            this.tlpAyuda.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.tlpAyuda.ToolTipTitle = "Mensaje de Ayuda";
             // 
             // frmCuadroNotas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tlpAyuda);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbRegistros);
             this.Controls.Add(this.gbPromedio);
             this.Controls.Add(this.gbNotas);
             this.Name = "frmCuadroNotas";
             this.Text = "Sistema de notas ITR";
             this.gbNotas.ResumeLayout(false);
             this.gbNotas.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.gbPromedio.ResumeLayout(false);
             this.gbPromedio.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.gbRegistros.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -247,21 +249,21 @@
 
         private System.Windows.Forms.GroupBox gbNotas;
         private System.Windows.Forms.GroupBox gbPromedio;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtNotaTres;
+        private System.Windows.Forms.TextBox txtNotaDos;
+        private System.Windows.Forms.TextBox txtNotaUno;
         private System.Windows.Forms.Label lblError;
         private System.Windows.Forms.Label lblNotaTres;
         private System.Windows.Forms.Label lblNotaDos;
         private System.Windows.Forms.Label lblNotaUno;
         private System.Windows.Forms.Label lblPromedio;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPromedioFinal;
+        private System.Windows.Forms.GroupBox gbRegistros;
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.Button btnNuevoRegistro;
         private System.Windows.Forms.Button btnAgregarPromedio;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.ToolStrip tlpAyuda;
+        private System.Windows.Forms.ToolTip tlpAyuda;
     }
 }
 
--- promedio/Form1.cs
+++ promedio/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,69 @@ namespace promedio
         public frmCuadroNotas()
         {
             InitializeComponent();
+        }
+
+        private void btnAgregarPromedio_Click(object sender, EventArgs e)
+        {
+            double nota1, nota2, nota3, promedio;
+            if (String.IsNullOrEmpty(txtNotaUno.Text) ||
+                    String.IsNullOrEmpty(txtNotaDos.Text) ||
+                    String.IsNullOrEmpty(txtNotaTres.Text)) 
+            {
+                lblError.Text = "Existe un campo vacio \n porfavor ingresa el dato que falta";
+            }
+            else
+            {
+                nota1 = double.Parse(txtNotaUno.Text);
+                nota2 = double.Parse(txtNotaDos.Text);
+                nota3 = double.Parse(txtNotaTres.Text);
+
+                if ((nota1 > 10 || nota1 < 0) || (nota2 > 10 || nota2 < 0) || (nota3 > 10 || nota3 < 0))
+                {
+                    lblError.Text = "Porfavor oingresa un valor entre 0 y 10";
+                }
+                else
+                {
+                    promedio = (nota1 + nota2 + nota3) / 3;
+                    txtPromedioFinal.Text = promedio.ToString("N2");
+                    lblError.Text = string.Empty;
+                }
+                
+            }
+            txtNotaUno.ReadOnly = true;
+            txtNotaDos.ReadOnly = true;
+            txtNotaTres.ReadOnly = true;
+
+
+            //double nota1, nota2, nota3, promedio;
+            //nota1 = double.Parse(txtNotaUno.Text);
+            //nota2 = double.Parse(txtNotaDos.Text);
+            //nota3 = double.Parse(txtNotaTres.Text);
+            //if (nota1 >0 && nota1 <=10 && nota2 > 0 && nota2 <= 10 && nota3 > 0 && nota3 <= 10)
+            //{
+            //    promedio = (nota1+ nota2 + nota3) / 3;
+            //    txtPromedioFinal.Text = promedio.ToString(); 
+            //}
+            //else
+            //{
+            //    txtPromedioFinal.Text = "Verifica que hayas ingresado notas entre 1 a 10";
+            //}
+        }
+
+        private void btnNuevoRegistro_Click(object sender, EventArgs e)
+        {
+            txtNotaUno.Clear();
+            txtNotaDos.Clear();
+            txtNotaTres.Clear();
+            txtPromedioFinal.Clear();
+            txtNotaUno.ReadOnly = false;
+            txtNotaDos.ReadOnly = false;
+            txtNotaTres.ReadOnly = false;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
